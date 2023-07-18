49d63f54480143e460b2f0a473530228ea53148a
LJS0000 <jisu2281l@naver.com>
2023-07-18 11:22:03 +0900

    ✨ FEAT: post view CRUD 완료

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    CreateView,
+    DetailView,
+    UpdateView,
+    DeleteView,
+)
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Post, Comment, Hashtag
 
-# Create your views here.
+
+### Post
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    paginate_by = 9
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+
+class PostWriteView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'blog/post_write.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    template_name = 'blog/blog_update.html'
+    fields = ['title', 'content']
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    success_url = '/'
