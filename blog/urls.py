ca126ae5047aa86e14ba09c8073841a3638278c8
LJS0000 <jisu2281l@naver.com>
2023-07-18 11:34:17 +0900

    ✨ FEAT: post URL 설정 완료

---
+++ blog/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    # 글 목록 조회
+    path("", views.PostListView.as_view(), name='list'),
+    # 글 상세 조회
+    path("detail/<int:pk>/", views.PostDetailView.as_view(), name='detail'),
+    # 글 작성
+    path("write/", views.PostWriteView.as_view(), name='write'),
+    # 글 수정
+    path("detail/<int:pk>/edit/", views.PostUpdateView.as_view(), name='edit'),
+    # 글 삭제
+    path("detail/<int:pk>/delete/", views.PostDeleteView.as_view(), name='delete'),
+]
