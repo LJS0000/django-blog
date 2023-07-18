1a8f769d9f6f69a65bd616d3bf111211c57b90bb
LJS0000 <jisu2281l@naver.com>
2023-07-18 10:35:03 +0900

    ✨ FEAT: blog, user Model 생성 완료

--- user/models.py
+++ user/models.py
@@ -1,22 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils import timezone
+from django.core.validators import RegexValidator
 
 
 class UserManager(BaseUserManager):
-    def _create_user(self, email, password, is_staff, is_superuser, **extra_fields):
+    def _create_user(
+        self,
+        email,
+        password,
+        username=None,
+        profile_img=None,
+        is_staff=False,
+        is_superuser=False,
+        **extra_fields
+    ):
         if not email:
             raise ValueError('User must have an email')
-        # now = timezone.now() #현재시간 -> UTC 기준
+
         now = timezone.localtime()
         email = self.normalize_email(email)
         user = self.model(
             email=email,
+            username=username,
+            profile_img=profile_img,
             is_staff=is_staff,
-            is_active=True,
             is_superuser=is_superuser,
-            last_login=now,
             date_joined=now,
+            updated_at=now,
             **extra_fields
         )
         user.set_password(password)
@@ -24,26 +35,45 @@ class UserManager(BaseUserManager):
         return user
 
     # create_user
-    def create_user(self, email, password, **extra_fields):
-        return self._create_user(email, password, False, False, **extra_fields)
+    def create_user(
+        self, email, password, username=None, profile_img=None, **extra_fields
+    ):
+        # 회원가입 시에만 email의 '@' 전까지의 값으로 설정
+        if not username:
+            username = email.split('@')[0]
+        return self._create_user(
+            email, password, username, profile_img, False, False, **extra_fields
+        )
 
     # create_superuser
-    def create_superuser(self, email, password, **extra_fields):
-        return self._create_user(email, password, True, True, **extra_fields)
+    def create_superuser(
+        self, email, password, username=None, profile_img=None, **extra_fields
+    ):
+        # 회원가입 시에만 email의 '@' 전까지의 값으로 설정
+        if not username:
+            username = email.split('@')[0]
+        return self._create_user(
+            email, password, username, profile_img, True, True, **extra_fields
+        )
 
 
 class User(AbstractUser):
-    username = None
-    email = models.EmailField(unique=True, max_length=255)
-    name = models.CharField(max_length=50, null=True, blank=True)
+    email = models.EmailField(unique=True, max_length=254)
+    username = models.CharField(unique=True, max_length=254)
+    profile_img = models.TextField(blank=True, null=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    last_login = models.DateTimeField(null=True, blank=True)
+    last_login = models.DateTimeField(auto_now=True)
     date_joined = models.DateTimeField(auto_now_add=True)
-
-    USERNAME_FIELD = 'email'
-    EMAIL_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    updated_at = models.DateTimeField(auto_now=True)
 
     objects = UserManager()
+
+    def set_username(self, username):
+        self.username = username
+        self.save()
+
+    def set_profile_img(self, profile_img):
+        self.profile_img = profile_img
+        self.save()
